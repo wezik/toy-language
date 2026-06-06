da208fb4c69d9021a90cdb68fc5c9731bd3ff3f0
wezik <wezik@users.noreply.github.com>
2026-06-06 12:24:59 +0200

    Interpreted and file run modes

--- src/main/kotlin/dev/wezik/toy/ToyApplication.kt
+++ src/main/kotlin/dev/wezik/toy/ToyApplication.kt
@@ -1,9 +1,29 @@
 package dev.wezik.toy
 
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
 
 fun main(args: Array<String>) {
-	val filePath = if (args.isNotEmpty()) args[0] else error("need file path")
-	val bytes = File(filePath).readBytes()
-	println(bytes.toString(Charsets.UTF_8))
+    if (args.isEmpty()) runInteractive() else runFile(args[0])
+}
+
+fun runInteractive() {
+    val input = InputStreamReader(System.`in`)
+    val reader = BufferedReader(input)
+
+    fun BufferedReader.readWithPrompt(): String? {
+        print("> ")
+        return reader.readLine()
+    }
+
+    while (true) {
+        val line = reader.readWithPrompt() ?: break
+        println("Interpreting: $line")
+    }
+}
+
+fun runFile(filePath: String) {
+    val bytes = File(filePath).readBytes()
+    println(String(bytes))
 }

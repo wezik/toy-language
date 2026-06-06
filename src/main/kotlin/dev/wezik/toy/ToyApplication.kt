bdd41d873caeebd828dbddff94461d4f6297b7e7
wezik <wezik@users.noreply.github.com>
2026-06-06 12:12:34 +0200

    Read bytes off file

--- src/main/kotlin/dev/wezik/toy/ToyApplication.kt
+++ src/main/kotlin/dev/wezik/toy/ToyApplication.kt
@@ -1,5 +1,9 @@
 package dev.wezik.toy
 
-fun main() {
-	println("Hello world")
+import java.io.File
+
+fun main(args: Array<String>) {
+	val filePath = if (args.isNotEmpty()) args[0] else error("need file path")
+	val bytes = File(filePath).readBytes()
+	println(bytes.toString(Charsets.UTF_8))
 }
